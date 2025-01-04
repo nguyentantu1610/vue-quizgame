ee41f4df37fff2ee26012e50baa64baa50e4d68e
nguyentantu1610 <tudeyer@gmail.com>
2025-01-04 18:03:08 +0700

    Config project

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,91 @@
-import './assets/main.css'
+import "./assets/style.css";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp, markRaw } from "vue";
+import { createPinia } from "pinia";
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
+import router from "./router";
 
-const app = createApp(App)
+import PrimeVue from "primevue/config";
+import Aura from "@primevue/themes/aura";
+import { definePreset } from "@primevue/themes";
+import FocusTrap from "primevue/focustrap";
+import Tooltip from "primevue/tooltip";
+import ConfirmationService from "primevue/confirmationservice";
+import ToastService from "primevue/toastservice";
+import "primeicons/primeicons.css";
 
-app.use(createPinia())
-app.use(router)
+const Noir = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: "{zinc.50}",
+      100: "{zinc.100}",
+      200: "{zinc.200}",
+      300: "{zinc.300}",
+      400: "{zinc.400}",
+      500: "{zinc.500}",
+      600: "{zinc.600}",
+      700: "{zinc.700}",
+      800: "{zinc.800}",
+      900: "{zinc.900}",
+      950: "{zinc.950}",
+    },
+    colorScheme: {
+      light: {
+        primary: {
+          color: "{zinc.950}",
+          inverseColor: "#ffffff",
+          hoverColor: "{zinc.900}",
+          activeColor: "{zinc.800}",
+        },
+        highlight: {
+          background: "{zinc.950}",
+          focusBackground: "{zinc.700}",
+          color: "#ffffff",
+          focusColor: "#ffffff",
+        },
+      },
+      dark: {
+        primary: {
+          color: "{zinc.50}",
+          inverseColor: "{zinc.950}",
+          hoverColor: "{zinc.100}",
+          activeColor: "{zinc.200}",
+        },
+        highlight: {
+          background: "rgba(250, 250, 250, .16)",
+          focusBackground: "rgba(250, 250, 250, .24)",
+          color: "rgba(255,255,255,.87)",
+          focusColor: "rgba(255,255,255,.87)",
+        },
+      },
+    },
+  },
+});
 
-app.mount('#app')
+const app = createApp(App);
+
+// Config and use Pinia
+const pinia = createPinia();
+pinia.use(({ store }) => {
+  store.router = markRaw(router);
+});
+app.use(pinia);
+
+// Use router
+app.use(router);
+
+// Config and use PrimeVue UI Component
+app.use(PrimeVue, {
+  theme: {
+    preset: Noir,
+    options: { darkModeSelector: ".dark" },
+  },
+  ripple: true,
+});
+app.use(ConfirmationService);
+app.use(ToastService);
+app.directive("focustrap", FocusTrap);
+app.directive("tooltip", Tooltip);
+
+app.mount("#app");
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: "selector",
+  content: ["./index.html", "./src/**/*.{vue,js,ts}"],
+  theme: { extend: {} },
+  plugins: [],
+};
